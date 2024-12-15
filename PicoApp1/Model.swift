ff4440af13051432c7550299fa2a41cd6f011e21
GHADAHIOS <ghada515alenzi515@gmail.com>
2024-12-15 10:28:26 +0300

    add

---
+++ PicoApp1/Model.swift
@@ -0,0 +1,7 @@
+//
+//  Model.swift
+//  PicoApp1
+//
+//  Created by GHADAH ALENEZI on 14/06/1446 AH.
+//
+
---
+++ PicoApp1/ViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  ViewModel.swift
+//  PicoApp1
+//
+//  Created by GHADAH ALENEZI on 14/06/1446 AH.
+//
+

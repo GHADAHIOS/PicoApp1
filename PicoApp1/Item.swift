57b7a01df304b5b8669f0e598ec167449d502be3
GHADAHIOS <ghada515alenzi515@gmail.com>
2024-12-15 09:56:28 +0300

    Initial Commit

---
+++ PicoApp1/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PicoApp1
+//
+//  Created by GHADAH ALENEZI on 14/06/1446 AH.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PicoApp1Tests/PicoApp1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PicoApp1Tests.swift
+//  PicoApp1Tests
+//
+//  Created by GHADAH ALENEZI on 14/06/1446 AH.
+//
+
+import Testing
+@testable import PicoApp1
+
+struct PicoApp1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

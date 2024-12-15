0edac7aac946831d598bad317e46740fc042f8f8
GHADAHIOS <ghada515alenzi515@gmail.com>
2024-12-15 13:01:50 +0300

    add

--- PicoApp1/View/CategoriesView.swift
+++ PicoApp1/View/CategoriesView.swift
@@ -13,7 +13,7 @@ struct CategoryCardView: View {
             Text(title)
                 .font(.largeTitle) // تغيير الخط إلى SF Arabic وحجم 50
             .fontWeight(.bold) // جعل النص Bold
-            .foregroundColor(.white) // لون الن
+            .foregroundColor(.white) // لون النص
             Image(imageName)
                 .resizable()
                 .scaledToFit()
@@ -32,46 +32,69 @@ struct CategoriesView: View {
     // استخدام ViewModel لتحميل البيانات
     @ObservedObject var viewModel = CategoriesViewModel()
     
+    @State private var isArabic: Bool = true // حالة لتحديد اللغة
     
-     var body: some View {
-         ZStack {
-             Color.BG.edgesIgnoringSafeArea(.all) // خلفية الصفحة
-             
-             VStack {
-                 // إضافة صورة قبل الفئات
-                 HStack {
-                     Spacer()
-                     Image("cloud") // الصورة التي تظهر قبل الفئات
-                         .resizable()
-                         .scaledToFit()
-                         .frame(width:724 , height: 126)
-                     
-                     
-                     Spacer()
-                 }
-                 
-                 Spacer()
-                 
-                 // عرض الفئات باستخدام ForEach
-                 HStack(spacing: 44) {
-                     ForEach(viewModel.categories) { category in
-                         CategoryCardView(
-                             title: category.title,
-                             imageName: category.imageName,
-                             color: category.color,
-                             shadowColor: category.shadowColor,
-                             shadowRadius: category.shadowRadius
-                         )
-                     }
-                 }
-                 
-                 Spacer()
-             }
-             
-         }
-     }
- }
-
+    var body: some View {
+        ZStack {
+            Color.BG.edgesIgnoringSafeArea(.all) // خلفية الصفحة
+            
+            VStack {
+                // إضافة صورة قبل الفئات
+                HStack {
+                    Button(action: {
+                        // تغيير اللغة عند الضغط على الزر
+                        isArabic.toggle()
+                    }) {
+                        ZStack {
+                            Circle()
+                                .fill(Color.inspire) // لون رمادي شفاف
+                                .frame(width: 100, height: 100) // حجم الدائرة
+                                .offset(x: 3, y: 3) // تحريك الدائرة لأسفل ولليمين قليلًا
+                            
+                            Circle()
+                                .fill(Color.binspire)
+                                .frame(width: 100, height: 100) // نفس الحجم كالدائرة السفلية
+                                .padding(.all, 5)
+                            // الأيقونة داخل الدائرة
+                            Image( systemName:"globe")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60, height: 60)
+                                .foregroundColor(.white) // لون ا
+                        }}
+                    .padding(.leading, 20) // إضافة مساحة من اليسار
+                    
+                    Spacer()
+                    
+                    Image("cloud") // الصورة التي تظهر قبل الفئات
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:724 , height: 126)
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                // عرض الفئات باستخدام ForEach
+                HStack(spacing: 44) {
+                    ForEach(viewModel.categories) { category in
+                        CategoryCardView(
+                            title: category.title,
+                            imageName: category.imageName,
+                            color: category.color,
+                            shadowColor: category.shadowColor,
+                            shadowRadius: category.shadowRadius
+                        )
+                    }
+                }
+                
+                Spacer()
+            }
+            
+        }
+    }
+}
 
 // المعاينة
 struct CategoriesView_Previews: PreviewProvider {

b71d2142d8b07c8581e3a0595742333a5fa92efa
ANOUD <a20nood@gmail.com>
2024-12-17 11:15:33 +0300

    add buttons to celebration screen

--- PicoApp1/CellarbrationScreen.swift
+++ PicoApp1/CellarbrationScreen.swift
@@ -12,62 +12,217 @@ struct CellarbrationScreen: View {
         ZStack{
             Color(.BG)
                 .ignoresSafeArea()
-            HStack{
-                ZStack {
-                    Circle()
-                        .fill(Color.inspire) // لون رمادي شفاف
-                        .frame(width: 100, height: 100) // حجم الدائرة
-                        .offset(x: 3, y: 3) // تحريك الدائرة لأسفل ولليمين قليلًا
-                    
-                    Circle()
-                        .fill(Color.binspire)
-                        .frame(width: 100, height: 100) // نفس الحجم كالدائرة السفلية
-                        .padding(.all, 5)
-                    // الأيقونة داخل الدائرة
-                    Image(systemName: "circle.grid.2x2")
+            VStack{
+                HStack{
+                    ZStack {
+                        Circle()
+                            .fill(Color.inspire) // لون رمادي شفاف
+                            .frame(width: 100, height: 100) // حجم الدائرة
+                            .offset(x: 3, y: 3) // تحريك الدائرة لأسفل ولليمين قليلًا
+                        
+                        Circle()
+                            .fill(Color.binspire)
+                            .frame(width: 100, height: 100) // نفس الحجم كالدائرة السفلية
+                            .padding(.all, 5)
+                        // الأيقونة داخل الدائرة
+                        Image(systemName: "circle.grid.2x2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white) // لون الأيقونة
+                    }
+                    .padding(.leading, 45.0)
+                    .overlay(
+                        Text("التلوين")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .padding(.leading, 45.0)
+                            .offset(y: 70) // تحريك النص تحت الدائرة
+                    )
+                    Spacer(minLength:12)
+                    ZStack {
+                        Circle()
+                            .fill(Color.inspire) // لون رمادي شفاف
+                            .frame(width: 100, height: 100) // حجم الدائرة
+                            .offset(x: 3, y: 3)
+                        
+                        Circle()
+                            .fill(Color.binspire)
+                            .frame(width: 100, height: 100) // نفس الحجم كالدائرة السفلية
+                        
+                        // الأيقونة داخل الدائرة
+                        Image(systemName: "paintbrush.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white) // لون الأيقونة
+                        
+                    }
+                    .padding(.trailing, 45.0)
+                    .overlay(
+                        Text("التلوين")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .padding(.trailing, 45.0)
+                            .offset(y: 70) // تحريك النص تحت الدائرة
+                    )
+                } //end of hstack
+                
+                // صورة السحابة مع الشخصية
+                HStack {
+                    Image("Pico")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.white) // لون الأيقونة
-                }
-                .padding(.leading, 45.0)
-                .overlay(
-                    Text("التلوين")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.black)
-                        .padding(.leading, 45.0)
-                        .offset(y: 70) // تحريك النص تحت الدائرة
-                )
-                Spacer(minLength:12)
-                ZStack {
-                    Circle()
-                        .fill(Color.inspire) // لون رمادي شفاف
-                        .frame(width: 100, height: 100) // حجم الدائرة
-                        .offset(x: 3, y: 3)
+                        .frame(width: 115, height: 115)
+                        .offset(x: -80, y: 50)
+
+                    ZStack {
+                        Image("cloud")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 880.0, height: 326)
+                            .offset(x: -80, y: -20)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.font1)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 50)
+                            .offset(x: -80, y: -20)
+                        
+                        Text( "Say the category you would like to color")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.font1)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 80)
+                            .offset(x: -125, y: -30)
                     
-                    Circle()
-                        .fill(Color.binspire)
-                        .frame(width: 100, height: 100) // نفس الحجم كالدائرة السفلية
-                    
-                    // الأيقونة داخل الدائرة
-                    Image(systemName: "paintbrush.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.white) // لون الأيقونة
-                    
-                }
-                .padding(.trailing, 45.0)
-                .overlay(
-                    Text("التلوين")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.black)
-                        .padding(.trailing, 45.0)
-                        .offset(y: 70) // تحريك النص تحت الدائرة
-                )
+                } //end of zstack
             }
+            .padding(.top, -30)
+                
+                
+                //button
+                ZStack {
+                    RoundedRectangle(cornerRadius: 18)
+                        .fill(Color.bhope) // لون رمادي شفاف
+                        .frame(width: 398, height: 90) // حجم الدائرة
+                        .offset(x: 4, y: 4)
+                    
+                    RoundedRectangle(cornerRadius: 18)
+                        .fill(Color.hope)                         .frame(width: 398, height:90)
+
+                HStack {
+                    
+                    Image(.image3)
+                        .resizable()
+                        .frame(width: 25.0, height: 25.0)
+                        .padding(.bottom, 50)
+                    HStack {
+                        Text( "Save")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 2)
+            
+                        
+                        Image(systemName: "square.and.arrow.down")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 2)
+                    }.padding(.horizontal, 80)
+        
+                    Image(.image3)
+                        .resizable()
+                        .frame(width: 25.0, height: 25.0)
+                        .padding(.bottom, 50)
+                    }
+                    
+                }
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 18)
+                        .fill(Color.inspire) // لون رمادي شفاف
+                        .frame(width: 398, height: 90) // حجم الدائرة
+                        .offset(x: 4, y: 4)
+                    
+                    RoundedRectangle(cornerRadius: 18)
+                        .fill(Color.binspire)                         .frame(width: 398, height:90)
+
+                HStack {
+                    
+                    Image(.image2)
+                        .resizable()
+                        .frame(width: 25.0, height: 25.0)
+                        .padding(.bottom, 50)
+                    HStack {
+                        Text( "Share")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 2)
+            
+                        
+                        Image(systemName: "square.and.arrow.up")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 2)
+                    }.padding(.horizontal, 80)
+        
+                    Image(.image2)
+                        .resizable()
+                        .frame(width: 25.0, height: 25.0)
+                        .padding(.bottom, 50)
+                    }
+                    
+                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 18)
+                        .fill(Color.bBrave) // لون رمادي شفاف
+                        .frame(width: 398, height: 90) // حجم الدائرة
+                        .offset(x: 4, y: 4)
+                    
+                    RoundedRectangle(cornerRadius: 18)
+                        .fill(Color.brave)                         .frame(width: 398, height:90)
+
+                HStack {
+                    
+                    Image(.image1)
+                        .resizable()
+                        .frame(width: 25.0, height: 25.0)
+                        .padding(.bottom, 50)
+                    HStack {
+                        Text( "Delete")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 2)
+            
+                        
+                        Image(systemName: "trash.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 2)
+                    }.padding(.horizontal, 80)
+        
+                    Image(.image1)
+                        .resizable()
+                        .frame(width: 25.0, height: 25.0)
+                        .padding(.bottom, 50)
+                    }
+                    
+                }
+
+            }
+           
             
         }
         

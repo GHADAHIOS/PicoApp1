f3857b772c8d2d9a6a0406a6e59bcfb1cfbfd189
ANOUD <a20nood@gmail.com>
2024-12-25 12:00:07 +0300

    add onboardingView

---
+++ PicoApp1/View/onboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  onboardingView.swift
+//  PicoApp1
+//
+//  Created by Alanoud Alamrani on 24/06/1446 AH.
+//
+
+import SwiftUI
+
+struct onboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    onboardingView()
+}
